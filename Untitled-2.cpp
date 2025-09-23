3c66a559abb4fb2f5dab0ac3013c451e2cb760ec
fasih <fasihulhassan486@gmail.com>
2025-09-23 05:47:46 -0700

    Add Fibonacci program in Untitled-2.cpp

---
+++ Untitled-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5; 
+    int a = 0, b = 1;
+    cout << "Fibonacci Series: ";
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+ return 0;
+}
